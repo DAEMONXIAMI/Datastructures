ab340b435349476b1f1ff9975937bbbe024363a8
wq <735462752@qq.com>
2019-03-08 21:07:57 +0800

    复习归并排序与快排

--- src/main/sort/Merge.java
+++ src/main/sort/Merge.java
@@ -1,37 +1,85 @@
 package main.sort;
 
 public class Merge {
-    public static int[] arraymirro;
+    /*    public static int[] arraymirro;
 
-    public static void sort(int[] array){
-        int lo = 0;
-        int hi = array.length-1;
-        sort(array, lo, hi);
+        public static void sort(int[] array) {
+            int lo = 0;
+            int hi = array.length - 1;
+            sort(array, lo, hi);
+        }
+
+        //递归调用
+        public static void sort(int[] array, int lo, int hi) {
+            int mid = (lo + hi) / 2;
+            if (lo >= hi) return;
+            sort(array, lo, mid);
+            sort(array, mid + 1, hi);
+            merge(array, lo, hi);
+        }
+
+        //把一个数组分成两半去排列，并且这两半已经排序好
+        public static void merge(int[] array, int lo, int hi) {
+            int mid = (lo + hi) / 2;
+            int length = hi - lo;
+            arraymirro = new int[array.length];
+            for (int i = lo; i <= hi; i++) {
+                arraymirro[i] = array[i];
+            }
+
+            for (int x = lo, i = lo, j = mid + 1; x <= hi; x++) {
+                if (i > mid) array[x] = arraymirro[j++];
+                else if (j > hi) array[x] = arraymirro[i++];
+                else if (arraymirro[i] < arraymirro[j]) array[x] = arraymirro[i++];
+                else array[x] = arraymirro[j++];
+            }
+        }*/
+    public void sort(int[] array) {
+        int l = 0;
+        int r = array.length - 1;
+        Sort(array, l, r);
     }
 
-    //递归调用
-    public static void sort(int[] array, int lo, int hi) {
-        int mid = (lo+hi)/2;
-        if(lo>=hi) return;
-        sort(array, lo, mid);
-        sort(array,mid+1 , hi);
-        merge(array, lo, hi);
+    public void Sort(int[] array, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int mid = (l + r) / 2;
+        Sort(array, l, mid);
+        Sort(array, mid + 1, r);
+        if (array[mid] > array[mid + 1])
+            mergeSort(array, l, mid, r);
     }
 
-    //把一个数组分成两半去排列，并且这两半已经排序好
-    public static void merge(int[] array, int lo, int hi) {
-        int mid = (lo + hi) / 2;
-        int length = hi - lo;
-        arraymirro = new int[array.length];
-        for(int i = lo; i<=hi; i++){
-            arraymirro[i] = array[i];
-         }
+    public void mergeSort(int[] array, int l, int mid, int r) {
+        int[] aux = new int[r - l + 1];
+        for (int i = l; i <= r; i++) {
+            aux[i - l] = array[i];
+        }
 
-        for(int x=lo, i =lo, j = mid+1; x<=hi; x++){
-            if(i>mid) array[x]= arraymirro[j++];
-            else if(j>hi) array[x] =arraymirro[i++];
-            else if(arraymirro[i]<arraymirro[j]) array[x] = arraymirro[i++];
-            else array[x] = arraymirro[j++];
+        int i = l;
+        int j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (j < mid - 1) {
+                aux[k] = array[i];
+                i++;
+            }
+            if (i > mid) {
+                aux[k] = array[j];
+                j++;
+            }
+            if (array[i] <= array[j]) {
+                aux[k] = array[i];
+                i++;
+            } else {
+                aux[k] = array[j];
+                j++;
+            }
+        }
+
+        for (i = l; i <= r; i++) {
+            array[i - l] = aux[i];
         }
     }
 }
--- src/main/sort/Quick.java
+++ src/main/sort/Quick.java
@@ -1,47 +1,81 @@
 package main.sort;
 
 public class Quick {
-    public static void sort(int[] array) {
-        sort(array, 0, array.length-1);
-    }
-
-    public static void sort(int[] array, int lo, int hi) {
-        if (lo >= hi) {
-            return;
+    /*    public static void sort(int[] array) {
+            sort(array, 0, array.length - 1);
         }
-        int j = partition(array, lo, hi);
 
-        sort(array, lo, j-1);
-        sort(array, j+1,hi);
-    }
-
-    public static int partition(int[] array, int lo, int hi) {
-        int i = lo;
-        int j = hi+1;
-        int comp = array[lo];
-
-        while (true) {
-            while (comp > array[++i]) {
-                if(i==hi) break;
+        public static void sort(int[] array, int lo, int hi) {
+            if (lo >= hi) {
+                return;
             }
-            while (comp < array[--j]) {
-                if(j==lo) break;
-            }
+            int j = partition(array, lo, hi);
 
-            if(i>=j)
-                break;
+            sort(array, lo, j - 1);
+            sort(array, j + 1, hi);
+        }
+
+        public static int partition(int[] array, int lo, int hi) {
+            int i = lo;
+            int j = hi + 1;
+            int comp = array[lo];
+
+            while (true) {
+                while (comp > array[++i]) {
+                    if (i == hi) break;
+                }
+                while (comp < array[--j]) {
+                    if (j == lo) break;
+                }
+
+                if (i >= j)
+                    break;
+
+                int temp;
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
 
             int temp;
-            temp = array[i];
-            array[i] = array[j];
+            temp = array[lo];
+            array[lo] = array[j];
             array[j] = temp;
+
+            return j;
+        }*/
+    public void quickSort(int[] array) {
+        int l = 0;
+        int r = array.length - 1;
+        quickSort(array, l, r);
+    }
+
+    public void quickSort(int[] array, int l, int r) {
+        if (l >= r) {
+            return;
         }
 
-        int temp;
-        temp = array[lo];
-        array[lo] = array[j];
-        array[j] =temp;
+        int p = partition(array, l, r);
+        quickSort(array, l, p - 1);
+        quickSort(array, p + 1, l);
+    }
 
+    public int partition(int[] array, int l, int r) {
+        int v = array[l];
+        int temp = 0;
+        //接下来要做的是 array[l+1...j] < v; arr[j+1...l] >= v
+        int j =l ;//j的位置处于要排序的头部以前，是一个数组外的位置,j表示当前小于v的最后一个数的下标，i表示下一个要比较的数
+        for (int i = l + 1; i <= r; i++) {
+            if (array[i] < v) {
+                temp = array[i];
+                array[i] = array[j+1];
+                array[j+1] = temp;
+                j++;
+            }
+        }
+        //将放在头部的v和j位置的元素交换
+        temp = array[j];
+        array[j] = v;
         return j;
     }
 }
